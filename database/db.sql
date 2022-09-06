3a385f54344a1fa4bea06f22a2cfdaef718ea7b2
seif <baazouziseif9@gmail.com>
2022-09-06 06:28:59 +0100

    init server and create database

---
+++ database/db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE todos;
+
+\c todos
+
+CREATE TABLE todos (
+    todoID UUID PRIMARY KEY,
+    title VARCHAR NOT NULL, 
+    body VARCHAR NOT NULL, 
+    userID UUID NOT NULL,
+    workspace UUID NOT NULL,
+    createdAt TIMESTAMP DEFAULT NOW() 
+);
+
+CREATE INDEX UserIdInex ON todos USING hash (userID);
+CREATE INDEX WorkspaceInex ON todos USING hash (workspace);
---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"todos-service/src/db"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	db.InitDataBase()
+	defer db.CloseDataBase()
+
+	app := fiber.New()
+	app.Use(cors.New())
+
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT")))
+}
